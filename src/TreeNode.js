1db1dff2acac49a0ab446694d5604761a9cfb9a7
Guilherme Gules <guilhermegules@gmail.com>
2022-05-21 11:54:44 -0300

    feat(TreeNode): adding navigation between left and right nodes

--- src/TreeNode.js
+++ src/TreeNode.js
@@ -1,6 +1,32 @@
 export default class TreeNode {
+  #LEFT = 0;
+  #RIGHT = 1;
+
   constructor(value) {
     this.value = value;
     this.children = [];
+    this.parent = null;
+  }
+
+  get left() {
+    return this.children[this.#LEFT];
+  }
+
+  get right() {
+    return this.children[this.#RIGHT];
+  }
+
+  set left(node) {
+    this.children[this.#LEFT] = node;
+    if (node) {
+      node.parent = this;
+    }
+  }
+
+  set right(node) {
+    this.children[this.#RIGHT] = node;
+    if (node) {
+      node.parent = this;
+    }
   }
 }
