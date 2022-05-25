922ca8c2611cdec8ff8b5b389becb6c5a23d6436
Guilherme Gules <guilhermegules@gmail.com>
2022-05-24 21:15:49 -0300

    feat: implementing the differents tree traversals

--- src/Tree.js
+++ src/Tree.js
@@ -5,30 +5,154 @@ export default class Tree {
     this.root = new TreeNode(key, value);
   }
 
-  *preOrderTraversal(node = this.root) {
-    yield node;
+  preOrderTraversal(node = this.root) {
+    if (node === null) return;
 
-    if (!node.children.length) return [];
+    if (node.left) this.preOrderTraversal(root.left);
 
-    for (const child of node.children) {
-      yield* this.preOrderTraversal(child);
+    if (node.right) this.preOrderTraversal(root.right);
+  }
+
+  *preOrderTraversalIterative(node = this.root) {
+    if (node === null) return;
+
+    const stack = [node];
+
+    while (stack.length) {
+      const item = stack.pop();
+
+      yield item;
+      // Left child is pushed after right one,
+      // since we want to print left child first
+      // hence it must be above right child in the stack
+      if (item.right) {
+        stack.push(item.right);
+      }
+
+      if (item.left) {
+        stack.push(item.left);
+      }
     }
   }
 
-  *postOrderTraversal(node = this.root) {
-    if (!node.children.length) return [];
+  postOrderTraversal(node = this.root) {
+    if (node === null) return;
 
-    for (let child of node.children) {
-      yield* this.postOrderTraversal(child);
-    }
+    if (node.left) this.postOrderTraversal(node.left);
 
-    yield node;
+    if (node.right) this.postOrderTraversal(node.right);
   }
 
-  insert(parentNodeKey, key, value = key) {
-    for (const node of this.preOrderTraversal()) {
+  postOrderTraversalIterative(node = this.root) {
+    if (node === null) return;
+
+    const stack = [node];
+    const result = [];
+
+    while (stack.length) {
+      const last = stack.pop();
+
+      result.push(last);
+
+      if (last.left) stack.push(last.left);
+
+      if (last.right) stack.push(last.right);
+    }
+
+    return result;
+  }
+
+  inOrderTraversal(node = this.root) {
+    if (node === null) return;
+
+    if (node.left) {
+      this.inOrderTraversal(node.left);
+    }
+
+    console.log(node);
+
+    if (node.right) {
+      this.inOrderTraversal(node.right);
+    }
+  }
+
+  inOrderTraversalIterative(node = this.root) {
+    if (node === null) return;
+
+    const stack = [];
+    let current = node;
+
+    while (stack.length || current) {
+      while (current) {
+        stack.push(current);
+        current = current.left;
+      }
+
+      const last = stack.pop();
+
+      console.log(last);
+
+      current = last.right;
+    }
+  }
+
+  breadthFirstTraversal(node = this.root) {
+    if (node === null) return;
+
+    const queue = [node];
+
+    while (queue.length) {
+      const item = queue.shift();
+
+      console.log(item);
+
+      if (item.left) {
+        queue.push(item.left);
+      }
+
+      if (item.right) {
+        queue.push(item.right);
+      }
+    }
+
+    return queue;
+  }
+
+  breadthFirstTraversalWithMatrix(node = this.root) {
+    if (node === null) return;
+
+    const queue = [node];
+    const tree = [];
+
+    while (queue.length) {
+      const length = queue.length;
+      const level = [];
+      for (let i = 0; i < length; i++) {
+        const item = queue.shift();
+
+        level.push(item);
+
+        if (item.left) {
+          queue.push(item.left);
+        }
+
+        if (item.right) {
+          queue.push(item.right);
+        }
+      }
+
+      tree.push(level);
+    }
+
+    return tree;
+  }
+
+  insert(parentNodeKey, key, value = key, left = null, right = null) {
+    for (const node of this.preOrderTraversalIterative()) {
       if (node.key === parentNodeKey) {
-        node.children.push(new TreeNode(key, value, node));
+        node.children.push(
+          new TreeNode({ key, value, parent: node, left, right })
+        );
 
         return true;
       }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import Tree from "./Tree.js";
 
-const tree = new Tree(1, "A");
+const tree = new Tree({ key: 1, value: "A", left: 2, right: 3 });
 
-tree.insert(1, 2, "AB");
-tree.insert(1, 3, "AC");
-tree.insert(1, 12, "AD");
-tree.insert(12, 121, "B");
+tree.insert(1, 2, "AB", 4, 5);
+tree.insert(1, 3, "AC", 6, 7);
 
-console.log([...tree.preOrderTraversal()].map((x) => x.value));
-// ['A', 'AB', 'AC', 'AD', 'B']
-
-console.log(tree.root.value); // 'A'
-console.log("Tree has children", tree.root.hasChildren); // true
-
-console.log("key 12 is a leaf", tree.find(12).isLeaf); // true
-console.log("key 121 is a leaf", tree.find(121).isLeaf); // false
-console.log(`parent value of the key 121 is ${tree.find(121).parent.value}`); // 'AD'
-
-tree.remove(12);
-
-console.log([...tree.postOrderTraversal()].map((x) => x.value));
-// ['A']
+console.log(tree.breadthFirstTraversal());
